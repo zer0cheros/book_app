99d0ab52d056d9c75f210868e3deb4c1489449cf
Zer0cheros <christian.wiss@optimaedu.fi>
2022-11-07 08:23:17 +0200

    dev

--- server.js
+++ server.js
@@ -5,11 +5,24 @@ const bcrypt = require('bcrypt')
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
 const {auth} = require('./auth')
+const multer = require('multer')
 server.use(express.static('public'))
+
 server.use(express.urlencoded())
 server.use(cookieParser())
 server.use('/home', auth)
 const salt = 10
+
+const uploads = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, './public/uploads')
+    },
+    filename: (req, file, cb) =>{
+        cb(null, String(Date.now()), + ' - ' + file.originalname)
+    }
+})
+
+const upload = multer({storage: uploads})
 
 const db1 = [
     {car: 'BMW', year: 1990},
@@ -20,10 +33,11 @@ const db1 = [
 
 server.set('view engine', 'ejs')
 
-server.get('/', (req, res)=>{
+server.get('/', async(req, res)=>{
+    const books = await db('books').select('*')  
     res.render('index', {
         user: 'Christian',
-        db: db1
+        db: books
     })
 })
 
@@ -49,11 +63,15 @@ server.post('/signup', async(req, res)=>{
     res.redirect('/login')
 })
 
+server.post('/home/addbook', upload.single('img'), async(req, res, next)=>{
+    res.send(`<img src="${req.file.path}" /><br>`)
+})
+
 server.post('/login', async(req, res)=>{
     const user = await db('user').select().where({email: req.body.email})
     const comparedPassword = bcrypt.compareSync(req.body.password, user[0].password)
     if(comparedPassword){
-        const token = await jwt.sign(user[0], 'christian',  {expiresIn: 60 * 60 * 1000,})
+        const token = await jwt.sign(user[0], 'christian',  {expiresIn: 60 * 60 * 1000})
         res.cookie('auth', token, {httpOnly: true, sameSite: 'strict', secure: true})
         res.redirect('/home')
         /*res.render('dashboard', {
